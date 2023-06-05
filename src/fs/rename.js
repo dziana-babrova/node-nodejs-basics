d9a1995019d21223893c6098f43e518e3b90a30f
Diana_Bobrova <dziana_babrova@epam.com>
2023-06-05 23:24:27 +0300

    feat: add solution for fs/rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,16 @@
+import { rename as renameFile } from 'fs/promises';
+import path from 'path';
+import * as url from 'url';
+
 const rename = async () => {
-    // Write your code here 
+    const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+    const oldName = path.join(__dirname, '/files/wrongFilename.txt');
+    const newName = path.join(__dirname, '/files/properFilename.md');
+    try {
+        await renameFile(oldName, newName);
+    } catch (e) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await rename();
