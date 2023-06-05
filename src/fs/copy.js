96d7090dc97fc6a43597182df44d105bcb2b4113
Diana_Bobrova <dziana_babrova@epam.com>
2023-06-05 23:24:07 +0300

    feat: add solution for fs/copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,17 @@
+import { cp, mkdir } from 'fs/promises';
+import path from 'path';
+import * as url from 'url';
+
 const copy = async () => {
-    // Write your code here 
+    const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+    const sourcePath = path.join(__dirname, '/files');
+    const destionationPath = path.join(__dirname, '/files_copy');
+    try {
+        await mkdir(destionationPath);
+        await cp(sourcePath, destionationPath, { recursive: true });
+    } catch(e) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -7,9 +7,9 @@ const create = async () => {
     const pathName = path.join(__dirname, '/files/fresh.txt');
     const content = 'I am fresh and young';
     try {
-      await writeFile(pathName, content, {flag: 'ax'});
+        await writeFile(pathName, content, {flag: 'ax'});
     } catch {
-      throw new Error('FS operation failed');
+        throw new Error('FS operation failed');
     }
 };
 
