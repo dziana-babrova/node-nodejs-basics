b8e7a7a917e716a965c4066b2bc0a533d5cad9f3
Diana_Bobrova <dziana_babrova@epam.com>
2023-06-06 23:08:58 +0300

    feat: add solution for fs/read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,16 @@
+import { readFile } from 'fs/promises';
+import path from 'path';
+import * as url from 'url';
+
 const read = async () => {
-    // Write your code here 
+    const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+    const pathName = path.join(__dirname, '/files/fileToRead.txt');
+    try {
+      const content = await readFile(pathName, 'utf8');
+      console.log(content);
+    } catch (e) {
+      throw new Error('FS operation failed');
+    }
 };
 
 await read();
