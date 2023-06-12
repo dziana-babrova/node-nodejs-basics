ea8d1c4f92bac3dc8b5252adfaef9a1a95c1b346
Diana_Bobrova <dziana_babrova@epam.com>
2023-06-12 21:47:04 +0300

    feat: add solution for streams/write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,15 @@
+import { createWriteStream } from 'fs';
+import path from 'path';
+import { stdin } from 'process';
+import * as url from 'url';
+
 const write = async () => {
-    // Write your code here 
+    const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+    const pathName = path.join(__dirname, '/files/fileToWrite.txt');
+    const stream = createWriteStream(pathName);
+    stdin.on('data', (chunk) => {
+        stream.write(chunk);
+    })
 };
 
 await write();
