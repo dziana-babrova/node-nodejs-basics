d0f425151eca4f28c347908dd23c681541c5a333
Diana_Bobrova <dziana_babrova@epam.com>
2023-06-12 16:21:36 +0300

    feat: add solution for hash/calcHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,14 @@
+import { createHash } from 'node:crypto';
+import { readFile } from 'fs/promises';
+import path from 'path';
+import * as url from 'url';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+  const pathName = path.join(__dirname, '/files/fileToCalculateHashFor.txt');
+  const content = await readFile(pathName, 'utf8');
+  const hash = createHash('sha256').update(content).digest('hex');
+  console.log(hash);
 };
 
 await calculateHash();
