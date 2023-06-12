dd511ecae484e35a50a5383e6892a6d6b3ada5d0
Diana_Bobrova <dziana_babrova@epam.com>
2023-06-12 21:34:16 +0300

    feat: add solution for streams/read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,15 @@
+import { createReadStream } from 'fs';
+import path from 'path';
+import { stdout } from 'process';
+import * as url from 'url';
+
 const read = async () => {
-    // Write your code here 
+    const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+    const pathName = path.join(__dirname, '/files/fileToRead.txt');
+    const fileStream = createReadStream(pathName, 'utf-8');
+    fileStream.on('data', (chunk) => {
+        stdout.write(chunk);
+    })
 };
 
 await read();
