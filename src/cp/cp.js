9526790d9ba6dfbd87da0e30ab53b5c36366369f
Diana_Bobrova <dziana_babrova@epam.com>
2023-06-14 17:44:16 +0300

    feat: add solution for cp/cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,12 @@
+import { spawn } from 'child_process';
+import path from 'path';
+import * as url from 'url';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+    const pathName = path.join(__dirname, '/files/script.js');
+
+    spawn('node', [pathName, args], { shell: false, stdio: 'inherit' });
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess('test');
