01eb49aca6b7e9f18b122bdaf518f1502dc1446e
Diana_Bobrova <dziana_babrova@epam.com>
2023-06-15 20:49:09 +0300

    refactor: replace cycle with map in wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,33 +1,23 @@
-import { Worker, isMainThread, parentPort } from "worker_threads";
+import { Worker } from 'worker_threads';
 import path from 'path';
 import * as url from 'url';
-import os from 'os';
+import {cpus} from 'os';
 
 const runService = (workerData) => {
-    return new Promise((resolve) => {
-        const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
-        const pathName = path.join(__dirname, '/worker.js');
+  const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+  const pathName = path.join(__dirname, '/worker.js');
 
-        if (isMainThread) {
-          const worker = new Worker(pathName, { workerData });
-            worker.on('message', (message) => resolve({ data: message, status: 'resolved' }));
-            worker.on('error', () => resolve({ data: null, status: 'error' }));
-        } else {
-          const data = workerData;
-          parentPort.postMessage(`You said \"${data}\".`);
-        }
-    })
-}
+  return new Promise((resolve) => {
+    const worker = new Worker(pathName, { workerData });
+    worker.on('message', (message) => resolve({ data: message, status: 'resolved' }));
+    worker.on('error', () => resolve({ data: null, status: 'error' }));
+  });
+};
 
 const performCalculations = async () => {
-    const result = [];
-    let index = 10;
-    for (const _core of os.cpus()) {
-      const workerResult = await runService(index);
-        result.push(workerResult);
-        index++;
-    }
-    console.log(result);
+  const numbers = cpus().map((_, index) => runService(10 + index));
+  const result = await Promise.all(numbers);
+  console.log(result);
 };
 
 await performCalculations();
