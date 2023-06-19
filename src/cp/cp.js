35b0955b2bed4a2813c8c495810a97ad20ded94e
Diana_Bobrova <dziana_babrova@epam.com>
2023-06-19 19:41:46 +0300

    refactor: replace spawn with fork in cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,12 +1,13 @@
-import { spawn } from 'child_process';
+import { spawn, fork } from 'child_process';
 import path from 'path';
 import * as url from 'url';
 
-const spawnChildProcess = async (args) => {
+const spawnChildProcess = async (...args) => {
   const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
   const pathName = path.join(__dirname, '/files/script.js');
 
-  spawn('node', [pathName, args], { shell: false, stdio: 'inherit' });
+  fork(pathName, args, { stdio: [0, 1, 2, 'ipc'] });
+
 };
 
-spawnChildProcess('test');
+spawnChildProcess('test', 'test');
